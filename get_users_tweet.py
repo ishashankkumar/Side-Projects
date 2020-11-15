1edaeccc452bca693e9ca069e3d7c7799e4a3dba
Shashank Kumar <43869829+ishashankkumar@users.noreply.github.com>
2020-11-14 20:32:55 -0800

    added file to retrieve tweets from a user matching text
    This code uses tweepy module to retrieve last 200 tweets from a tweet user that matches the given string.

---
+++ get_users_tweet.py
@@ -0,0 +1,33 @@
+import tweepy
+
+
+USER_ID="chamath"
+TEXT_TO_GREP="investing"
+
+
+def get_twitter_auth():
+    # Create a profile on https://developer.twitter.com/ and copy the 
+    # consumer_key, consumer_secret, access_token, access_secret.
+    consumer_key = <consumer_key>
+    consumer_secret = <consumer_secret>
+    access_token = <access_token>
+    access_secret = <access_secret>
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    return auth
+
+
+def get_twitter_client():
+    auth = get_twitter_auth()
+    twitter_client = tweepy.API(auth, wait_on_rate_limit=True)
+    return twitter_client
+
+
+if __name__ == "__main__":
+    client = get_twitter_client()
+    for tweet in tweepy.Cursor(client.user_timeline, screen_name=USER_ID,
+                               count=None, since_id=None, max_id=None, trim_user=False,
+                               exclude_replies=True, contributor_details=False,
+                               include_entities=False, include_rts=False).items(200):
+        if TEXT_TO_GREP in tweet.text:
+            print(f"{tweet.text} \n")
